bba2273848c0ae3866ff479ed1499d3d7fd5f3f6
Fazil-Git-1 <afazilefe@gmail.com>
2026-08-12 00:51:43 +0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -97,7 +97,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       
       /* --- PWA ve Tema Meta Ayarları --- */
       { name: "theme-color", content: "#064e3b" },
-      { name: "apple-mobile-web-app-capable", content="yes" },
+      { name: "apple-mobile-web-app-capable", content: "yes" },
       { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" },
       { name: "apple-mobile-web-app-title", content: "Namaz Yarışması" }
     ],
